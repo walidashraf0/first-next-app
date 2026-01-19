5e65ec6dfa077f08159bf4241583c74b451e298e
Walid Ashraf <walidashraf050@gmail.com>
2026-01-19 19:59:32 +0200

    get single post with prisma

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@/lib/prisma";
 import { posts } from "@/utils/data";
 import { IUpdatePostDTO } from "@/utils/types";
 import { NextRequest, NextResponse } from "next/server";
@@ -6,8 +7,10 @@ interface IGetPostProps {
   params: { id: string };
 }
 
-export const GET = (request: NextRequest, { params }: IGetPostProps) => {
-  const post = posts.find((p) => p.id === parseInt(params.id));
+export const GET = async (request: NextRequest, { params }: IGetPostProps) => {
+  const post = await prisma.post.findUnique({
+    where: { id: parseInt(params.id) },
+  });
   if (!post) {
     return NextResponse.json({ message: "Post not found" }, { status: 404 });
   }
@@ -24,7 +27,10 @@ export const PUT = async (request: NextRequest, { params }: IGetPostProps) => {
   return NextResponse.json({ message: "Post Updated" }, { status: 200 });
 };
 
-export const DELETE = async (request: NextRequest, { params }: IGetPostProps) => {
+export const DELETE = async (
+  request: NextRequest,
+  { params }: IGetPostProps,
+) => {
   const { id } = await params;
   const post = posts.find((p) => p.id === parseInt(id));
   if (!post) {
