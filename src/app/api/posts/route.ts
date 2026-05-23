3c1ea56dbb1c008c03c0fa835eef557c0ec765a2
Walid Ashraf <walidashraf050@gmail.com>
2026-05-23 20:44:17 +0300

    Pagination

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -4,38 +4,56 @@ import { ICreatePostDTO } from "@/utils/types";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
-export const GET = async () => {
-  const allPosts = await prisma.post.findMany();
+export const GET = async (request: NextRequest) => {
+  try {
+    const pageNumber = request.nextUrl.searchParams.get("page") || "1";
+    const POSTS_PER_PAGE = 6;
+    const allPosts = await prisma.post.findMany({
+      skip: (parseInt(pageNumber) - 1) * POSTS_PER_PAGE,
+      take: POSTS_PER_PAGE,
+    });
 
-  return NextResponse.json(allPosts, { status: 200 });
+    return NextResponse.json(allPosts, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error!" },
+      { status: 500 },
+    );
+  }
 };
 
 export const POST = async (request: NextRequest) => {
-  const body = (await request.json()) as ICreatePostDTO;
+  try {
+    const body = (await request.json()) as ICreatePostDTO;
 
-  const createPostSchema = z.object({
-    title: z
-      .string()
-      .min(5, "Title must be at least 5 characters long")
-      .max(100),
-    content: z.string().min(10),
-  });
+    const createPostSchema = z.object({
+      title: z
+        .string()
+        .min(5, "Title must be at least 5 characters long")
+        .max(100),
+      content: z.string().min(10),
+    });
 
-  const validation = createPostSchema.safeParse(body);
+    const validation = createPostSchema.safeParse(body);
 
-  if (!validation.success) {
-    console.log(validation.error.issues[0].message);
-    return NextResponse.json(
-      { message: validation.error.issues[0].message },
-      { status: 400 },
-    );
-  }
+    if (!validation.success) {
+      console.log(validation.error.issues[0].message);
+      return NextResponse.json(
+        { message: validation.error.issues[0].message },
+        { status: 400 },
+      );
+    }
 
-  const newPost: Post = await prisma.post.create({
-    data: {
-      title: body.title,
-      content: body.content,
-    },
-  });
-  return NextResponse.json(newPost, { status: 201 });
+    const newPost: Post = await prisma.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+      },
+    });
+    return NextResponse.json(newPost, { status: 201 });
+  } catch (error) {}
+  return NextResponse.json(
+    { message: "Internal Server Error!" },
+    { status: 500 },
+  );
 };
